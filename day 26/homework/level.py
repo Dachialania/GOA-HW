f7f83950a8c5b0e68341b7367e418ad0ea57db8b
Dachialania <alaniadachi25@gmail.com>
2025-01-20 00:11:32 +0400

    commit

---
+++ day 26/homework/level.py
@@ -0,0 +1,45 @@
+# def sentence_to_list_and_back(sentence):
+#     words = sentence.split()
+#     new_sentence = ", ".join(words)
+    
+#     return new_sentence
+
+
+# print(sentence_to_list_and_back("Hello World Python is great"))
+
+
+
+# def print_word_lengths(sentence):
+ 
+#     words = sentence.split()
+
+#     for word in words:
+#         print(word)
+
+
+# print_word_lengths("Hello World Python is great")
+
+
+
+# def remove_extra_spaces(sentence):
+#     new_sentence = " ".join(sentence.split())   
+#     return new_sentence
+# print(remove_extra_spaces("Hello    World   Python   is    great"))
+
+
+# def replace_space_with_dash(sentence):
+#     new_sentence = "-".join(sentence.split())
+#     return new_sentence
+# print(replace_space_with_dash("Hello World Python is great"))
+
+
+# def reverse_words_in_sentence(sentence):
+#     words = sentence.split()
+#     reversed_sentence = " ".join(reversed(words))
+#     return reversed_sentence
+# print(reverse_words_in_sentence("Hello World"))
+
+
+
+# word = "hello"
+# print(word.capitalize()) 
---
+++ day 30/homework/level.py
@@ -0,0 +1,33 @@
+
+# my_list = [1, 2, 3, 4, 5]
+# del my_list[2]
+
+# my_list.insert(0, 'new_element')
+
+# print(my_list)  
+
+
+
+# def manual_len(item):
+#     length = 0
+#     for _ in item:
+#         length += 1
+#     return length
+
+# print(manual_len([1, 2, 3, 4]))  
+# print(manual_len("hello"))       
+
+
+
+# def manual_pop(my_list, index):
+    
+#     new_list = []
+    
+
+#     for i in range(len(my_list)):
+#         if i != index:
+#             new_list.append(my_list[i])
+    
+#     return new_list
+
+# print(manual_pop([1, 2, 3, 4, 5], 2))  
