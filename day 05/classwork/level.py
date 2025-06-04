a5ae37e14a8022f2ca9f42b274e91f9367070381
Dachialania <alaniadachi25@gmail.com>
2025-06-05 01:08:21 +0400

    commit

---
+++ day 05/classwork/level.py
@@ -0,0 +1,26 @@
+# გაასწორეთ შეცდომები და კომენტარის სახით მიუწერეთ თითოეულს თუ რა არის შეცდომა.
+
+# 1) print"lika"
+
+# print("lika")
+
+# 2)analyst_salary = 3000
+#  print(Analyst_salary)
+
+# analyst_salary = 3000
+#  print(analyst_salary)
+
+
+# n1 = 27
+# n2 = "12"
+# print(n1 / n2)
+
+# n1 = 27
+# n2 = 12
+# print(n1 / n2)
+ 
+# Status = mentor"
+# print(status)
+
+# Status = "mentor"
+# print(Status)
